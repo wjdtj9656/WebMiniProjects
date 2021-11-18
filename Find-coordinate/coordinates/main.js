44d776f1ea7de5b0242883a00f61f4ba2be957c3
wjdtj9656 <wjdtj9656@naver.com>
2021-11-18 18:36:06 +0900

    upgrade performance(css trigger)

--- Find-coordinate/coordinates/main.js
+++ Find-coordinate/coordinates/main.js
@@ -3,23 +3,30 @@ const horizontal = document.querySelector('.horizontal');
 const target = document.querySelector('.target');
 const tag = document.querySelector('.tag');
 
+addEventListener('load', () => {
+    const targetRect = target.getBoundingClientRect();
+    const targetHalfWidth = targetRect.width / 2;
+    const targetHalfHeight = targetRect.height / 2;
+
 document.addEventListener('mousemove', (event) => {
     const x = event.clientX;
     const y = event.clientY;
     const maxX = window.innerWidth;
     const maxY = window.innerHeight;
-    vertical.style.left = `${x}px`;
-    horizontal.style.top = `${y}px`;
-    target.style.left = `${x}px`;
-    target.style.top = `${y}px`;
-    tag.style.left = `${x}px`;
-    tag.style.top = `${y}px`;
+    vertical.style.transform = `translateX(${x}px)`;
+    horizontal.style.transform = `translateY(${y}px)`;
+    target.style.transform = `translate(${x - targetHalfWidth}px, ${y - targetHalfHeight}px)`;
+    tag.style.transform = `translate(${x}px, ${y}px)`;
+    
     tag.innerHTML =`(${x}px, ${y}px)`;
-    if(x / maxX > 0.8){
-        tag.style.left = `${x-100}px`;
-        tag.style.top = `${y-60}px`;
+    if(x / maxX > 0.8 && y / maxY < 0.2){
+        tag.style.transform = `translate(${x - 100}px,${y}px)`
     }
-    if(y / maxY > 0.8){
-        tag.style.top = `${y-60}px`;
+    else if(x / maxX > 0.8){
+        tag.style.transform = `translate(${x - 60}px, ${y - 50}px)`;
+    }
+    else if(y / maxY > 0.8){
+        tag.style.transform = `translate(${x}px, ${y-60}px)`;
     }
 });
+})
