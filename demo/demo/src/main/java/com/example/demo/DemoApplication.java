bfe1220a61004572bc3b9a8772a9737dad03f0a1
wjdtj9656 <wjdtj9656@naver.com>
2021-12-13 08:54:53 +0900

    backend initial setting

---
+++ demo/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//스프링부트를 설정하는 클래스임을 의미.
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
