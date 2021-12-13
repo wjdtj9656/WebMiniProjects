dcade4286b567e93041e3d90b31c8c6f632c1b30
wjdtj9656 <wjdtj9656@naver.com>
2021-12-13 18:00:44 +0900

    backend controller ex

---
+++ demo/demo/src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,58 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.TestRequestBodyDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+
+	@GetMapping("/testGetMapping")
+	public String testController() {
+		return "Hello world testGetMapping";
+	}
+	
+	@GetMapping("/{id}")
+	public String testCongrollerWithPathVariables(@PathVariable(required = false) int id)
+	{
+		return "Hello world! ID" + id;
+	}
+	
+	@GetMapping("/testRequestParam")
+	public String testControllerRequestParam(@RequestParam(required = false) int id) {
+		return "Hello world! ID" + id;
+	}
+	
+	@GetMapping("/testRequestBody")
+	public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {
+		return "Hello world ID" + testRequestBodyDTO.getId() + " Message: " + testRequestBodyDTO.getMessage();
+	}
+	
+	@GetMapping("/testResponseBody")
+	public ResponseDTO<String> testControllerResponseBody() {
+		List<String> list = new ArrayList<>();
+		list.add("Hello world! I'm responseDTO");
+		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+		return response;
+	}
+	
+	@GetMapping("/testResponseEntity")
+	public ResponseEntity<?> testControllerResponseEntity() {
+		List<String> list = new ArrayList<>();
+		list.add("Hello world! I'm responseEntity. And you got 400!");
+		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+		//http status 400으로 설정
+		return ResponseEntity.badRequest().body(response);
+	}
+}
--- demo/demo/src/main/java/com/example/demo/dto/ResponseDTO.java
+++ demo/demo/src/main/java/com/example/demo/dto/ResponseDTO.java
@@ -6,6 +6,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class ResponseDTO<T> {
 	private String error;
 	private List<T> data;
