6763294d337c91f73907a56bbc42b89363902a59
wjdtj9656 <wjdtj9656@naver.com>
2021-12-14 22:42:17 +0900

    error rollback

--- demo/demo/src/main/java/com/example/demo/controller/TodoController.java
+++ demo/demo/src/main/java/com/example/demo/controller/TodoController.java
@@ -34,30 +34,21 @@ public ResponseEntity<?> testTodo(){
 	ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
 	return ResponseEntity.ok().body(response);
 }
-@PostMapping
-public ResponseEntity<?> createTodo(@RequestBody TodoDTO dto){
-	try {
-		String temporaryUserId = "temporary-user";
-		
-		//TodoEntity로 변환
-		TodoEntity entity = TodoDTO.toEntity(dto);
-		//id를 NULL로 초기화한다. 생성 당시에는 id가 없어야 하기 때문이다.
-		entity.setId(null);
-		//임시 사용자 아이디 설정 - 4장에서 추가 구현
-		entity.setUserId(temporaryUserId);
-		//서비스를 이용해 Todo 엔티티를 생성한다.
-		List<TodoEntity> entities = service.create(entity);
-		//자바 스트림을 이용해 리턴된 엔티티 리스트를 TodoDTO 리스트로 변환한다.
-		List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
-		//변환된 TodoDTO 리스트를 이용해 ResponseDTO를 초기화한다.
-		ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
-		//responseDTO리턴
-		return ResponseEntity.ok().body(response);
-	} catch (Exception e) {
-		//혹시 예외가 있는경우 DTO대신 에러메시지 리턴
-		String error = e.getMessage();
-		ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
-		return ResponseEntity.badRequest().body(response);
-	}
-}
+/*
+ * @PostMapping public ResponseEntity<?> createTodo(@RequestBody TodoDTO dto){
+ * try { String temporaryUserId = "temporary-user";
+ * 
+ * //TodoEntity로 변환 TodoEntity entity = TodoDTO.toEntity(dto); //id를 NULL로
+ * 초기화한다. 생성 당시에는 id가 없어야 하기 때문이다. entity.setId(null); //임시 사용자 아이디 설정 - 4장에서 추가
+ * 구현 entity.setUserId(temporaryUserId); //서비스를 이용해 Todo 엔티티를 생성한다.
+ * List<TodoEntity> entities = service.create(entity); //자바 스트림을 이용해 리턴된 엔티티
+ * 리스트를 TodoDTO 리스트로 변환한다. List<TodoDTO> dtos =
+ * entities.stream().map(TodoDTO::new).collect(Collectors.toList()); //변환된
+ * TodoDTO 리스트를 이용해 ResponseDTO를 초기화한다. ResponseDTO<TodoDTO> response =
+ * ResponseDTO.<TodoDTO>builder().data(dtos).build(); //responseDTO리턴 return
+ * ResponseEntity.ok().body(response); } catch (Exception e) { //혹시 예외가 있는경우
+ * DTO대신 에러메시지 리턴 String error = e.getMessage(); ResponseDTO<TodoDTO> response =
+ * ResponseDTO.<TodoDTO>builder().error(error).build(); return
+ * ResponseEntity.badRequest().body(response); } }
+ */
 }
--- demo/demo/src/main/java/com/example/demo/service/TodoService.java
+++ demo/demo/src/main/java/com/example/demo/service/TodoService.java
@@ -3,49 +3,15 @@ package com.example.demo.service;
 
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import com.example.demo.model.TodoEntity;
-import com.example.demo.persistence.TodoRepository;
-
-import lombok.extern.slf4j.Slf4j;
 
 
-@Slf4j
 @Service
 public class TodoService {
 
-	@Autowired
-	private TodoRepository repository;
 	public String testService() {
-		//TodoEntity 생성
-		TodoEntity entity = TodoEntity.builder().title("My first todo item").build();
-		//TodoEntity 저장
-		repository.save(entity);
-		//TodoEntity 검색
-		TodoEntity savedEntity = repository.findById(entity.getId()).get();
-		return savedEntity.getTitle();
+		return "TEST SERVICE";
 	}
-	public List<TodoEntity> create(final TodoEntity entity) {
-		//Validation
-		validate(entity);
-		
-		
-		repository.save(entity);
-		
-		log.info("Entity Id : {} is saved.", entity.getId());
-		
-		return repository.findByUserId(entity.getUserId());
-	}
-	private void validate(final TodoEntity entity) {
-		if(entity == null) {
-			log.warn("Entity cannot be null");
-			throw new RuntimeException("Entity cannot be null");
-		}
-		if(entity.getUserId() == null) {
-			log.warn("unknown user");
-			throw new RuntimeException("unknown user.");
-		}
-	}
+	
 }
