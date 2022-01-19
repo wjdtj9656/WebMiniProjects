92040c1b607a5b4ac0b272756dfdc39dd4a8e117
wjdtj9656 <wjdtj9656@naver.com>
2022-01-20 00:03:00 +0900

    backend + Frontend

--- demo/react-workspace/todo-react-app/src/App.js
+++ demo/react-workspace/todo-react-app/src/App.js
@@ -1,58 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
-import Todo from './Todo';
-import React from 'react';
-import {Paper, List, Container} from "@material-ui/core";
+import React from "react";
+import Todo from "./Todo";
 import AddTodo from "./AddTodo.js";
+import { Paper, List, Container } from "@material-ui/core";
+import "./App.css";
+import { call } from "./service/ApiService";
+
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      items: [
-        { id: "0", title: "Hello World 1", done: true},
-        { id: "1", title: "Hello World 2", done: false},
-      ],
+      items: [],
     };
   }
 
-  //+버튼 함수 생성
-  add = (item) => {
-    const thisItems = this.state.items;
-    item.id = "ID-" + thisItems.length;
-    item.done = false;
-    thisItems.push(item);
-    this.setState({ items: thisItems});
-    console.log("items :", this.state.items);
+  componentDidMount() {
+    call("/todo", "GET", null).then((response) =>
+      this.setState({ items: response.data })
+    );
   }
 
-  //delete 함수
+  add = (item) => {
+    call("/todo", "POST", item).then((response) =>
+      this.setState({ items: response.data })
+    );
+  };
+
   delete = (item) => {
-    const thisItems = this.state.items;
-      console.log("Before Update Items:", this.state.items);
-    const newItems = thisItems.filter(e => e.id !== item.id);
-    this.setState({ items: newItems}, () => {
-      console.log("Update Items : ", this.state.items);
-    })
-  }
+    call("/todo", "DELETE", item).then((response) =>
+      this.setState({ items: response.data })
+    );
+  };
+
+  update = (item) => {
+    call("/todo", "PUT", item).then((response) =>
+      this.setState({ items: response.data })
+    );
+  };
+
   render() {
     var todoItems = this.state.items.length > 0 && (
-      <Paper style={{margin:16}}>
+      <Paper style={{ margin: 16 }}>
         <List>
-          {this.state.items.map((item, idx)=> (
-            <Todo item={item} key={item.id} delete={this.delete}/>
+          {this.state.items.map((item, idx) => (
+            <Todo
+              item={item}
+              key={item.id}
+              delete={this.delete}
+              update={this.update}
+            />
           ))}
         </List>
       </Paper>
-    )
-    //연결
+    );
+
+    // 3. props로 넘겨주기
     return (
       <div className="App">
         <Container maxWidth="md">
-          <AddTodo add={this.add}/>
+          <AddTodo add={this.add} />
           <div className="TodoList">{todoItems}</div>
         </Container>
       </div>
     );
   }
 }
+
 export default App;
