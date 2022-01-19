6fae4c37e913cfa9373d404996d39923813d69f6
wjdtj9656 <wjdtj9656@naver.com>
2022-01-19 20:57:41 +0900

    delete Todolist (FE)

--- demo/react-workspace/todo-react-app/src/Todo.js
+++ demo/react-workspace/todo-react-app/src/Todo.js
@@ -1,25 +1,49 @@
 import React from 'react';
-import {ListItem, ListItemText, InputBase, Checkbox} from "@material-ui/core";
+import {
+        ListItem,
+        ListItemText, 
+        InputBase, 
+        Checkbox,
+        ListItemSecondaryAction,
+        IconButton
+    } from "@material-ui/core";
+import DeleteOutlined from "@material-ui/icons/DeleteOutlined";
 class Todo extends React.Component {
     constructor(props){
         super(props);
         this.state = {item: props.item};
+        this.delete = props.delete;
+    }
+    //제거함수
+    deleteEventHandler = () => {
+        this.delete(this.state.item);
     }
     render() {
         const item = this.state.item;
         return (
             <ListItem>
+
                 <Checkbox checked={item.done} />
                 <ListItemText>
                     <InputBase
                         inputProps ={{ "aria-label": "naked"}}
                         type = "text"
                         id = {item.id}
+                        name = {item.id}
                         value = {item.title}
                         multiline = {true}
                         fullWidth = {true}
                     />
                 </ListItemText>
+
+                <ListItemSecondaryAction>
+                    <IconButton 
+                        aria-label="Delete Todo"
+                        onClick={this.deleteEventHandler}>
+                        <DeleteOutlined/>
+                    </IconButton>
+                </ListItemSecondaryAction>
+
             </ListItem>
         );
     }
