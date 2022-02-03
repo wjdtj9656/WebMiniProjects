f6baa862e639dcde44a6410c51d4970505baf613
wjdtj9656 <wjdtj9656@naver.com>
2022-02-03 23:10:32 +0900

    state(habits.jsx) -> state(App.jsx)

--- habit-tracker/src/app.jsx
+++ habit-tracker/src/app.jsx
@@ -1,9 +1,50 @@
 import './app.css';
+import React, {Component} from 'react';
 import Habits from './components/habits'
-function app() {
-  return <Habits>
-    
-  </Habits>;
-}
 
+
+class app extends Component{
+  state = {
+    habits: [
+        {id: 1, name: 'Reading', count: 0},
+        {id: 2, name: 'Running', count: 0},
+        {id: 3,name: 'Coding', count: 0},
+    ],
+}
+handleIncrement = (habit) => {
+  const habits = this.state.habits.map(item => {
+      if(item.id === habit.id){
+          return {...habit, count: habit.count+1};
+      }
+      return item;
+  })
+  this.setState({habits});
+}
+handleDecrement = (habit) => {
+  const habits = this.state.habits.map(item => {
+      if(item.id === habit.id && habit.count > 0){
+          return { ...habit, count: habit.count-1};
+      }
+      return item;
+  })
+  this.setState({habits});
+}
+handleDelete = (habit) => {
+  const habits = this.state.habits.filter(item => {
+      if(item.id !== habit.id){
+          return {...habit}
+      }
+  })
+  this.setState({habits});
+}
+  render(){
+    return(
+        <Habits habits={this.state.habits}
+        onIncrement={this.handleIncrement}
+        onDecrement={this.handleDecrement}
+        onDelete={this.handleDelete} 
+        />
+    );
+  }
+}
 export default app;
--- habit-tracker/src/components/habits.jsx
+++ habit-tracker/src/components/habits.jsx
@@ -1,50 +1,20 @@
 import React, { Component } from 'react';
 import Habit from './habit';
 class Habits extends Component {
-    state = {
-        habits: [
-            {id: 1, name: 'Reading', count: 0},
-            {id: 2, name: 'Running', count: 0},
-            {id: 3,name: 'Coding', count: 0},
-        ],
-    }
     handleIncrement = (habit) => {
-        const habits = this.state.habits.map(item => {
-            if(item.id === habit.id){
-                return {...habit, count: habit.count+1};
-            }
-            return item;
-        })
-        this.setState(
-            {habits}
-        );
+        this.props.onIncrement(habit);
     }
     handleDecrement = (habit) => {
-        const habits = this.state.habits.map(item => {
-            if(item.id === habit.id && habit.count > 0){
-                return { ...habit, count: habit.count-1};
-            }
-            return item;
-        })
-        this.setState(
-            {habits}
-        )
+        this.props.onDecrement(habit);
     }
     handleDelete = (habit) => {
-        const habits = this.state.habits.filter(item => {
-            if(item.id !== habit.id){
-                return {...habit}
-            }
-        })
-        this.setState(
-            {habits}
-        )
+        this.props.onDelete(habit);
     }
     render() {
         return (
             <ul>
                 {
-                    this.state.habits.map(habit => (
+                    this.props.habits.map(habit => (
                         <Habit key={habit.id} habit={habit} 
                         onIncrement={this.handleIncrement}
                         onDecrement={this.handleDecrement}
