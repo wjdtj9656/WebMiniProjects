b51206d9178bb9d2e116c961b00903d030a855d6
wjdtj9656 <wjdtj9656@naver.com>
2022-02-10 00:32:04 +0900

    youtube api fetch & show titleList

--- myYoutube/src/app.jsx
+++ myYoutube/src/app.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './app.css';
+import VideoList from './components/videoList/videoList';
 import Navbar from './components/searchHeader/navbar';
-function app() {
+function App() {
+  
+  const [videos,setVideos] = useState([]);
+  
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCPi39u5ZGiOUhDRYkdc084fy5DmIFJs3Y", requestOptions)
+      .then(response => response.json())
+      .then(result => console.log(setVideos(result.items)))
+      .catch(error => console.log('error', error));
+  }, []);
   return (
+    <>
     <Navbar />
+    <VideoList videos={videos}/>
+    </>
   );
 }
 
-export default app;
+export default App;
