84adb4f0097465bc593dbfe1bb920162ee50854f
wjdtj9656 <wjdtj9656@naver.com>
2022-02-10 22:49:27 +0900

    show video list

--- myYoutube/src/components/videoItem/videoItem.jsx
+++ myYoutube/src/components/videoItem/videoItem.jsx
@@ -1,5 +1,28 @@
 import React from 'react';
-
-const VideoItem = props => <h1>{props.video.snippet.title}</h1>
-
+import styles from './videoItem.module.css'
+const VideoItem = props => {
+    let title = props.video.snippet.title;
+    if(title.length > 50){
+        title = title.substring(0,50);
+        title += "....";
+    }
+    return(
+        <>
+        <div className={styles.videoBlock}>
+            <img 
+            className={styles.thumbnails} 
+            src = {props.video.snippet.thumbnails.default.url}
+            alt = "thumbnails"/>
+            <div className={styles.thumbnailsText}>
+                <div className={styles.thumbsnailsTitle}>
+                    <h4>{title}</h4>
+                </div>
+                <div className={styles.thumbnailsAuthor}>
+                    <h5>{props.video.snippet.channelTitle}</h5>
+                </div>
+            </div>
+        </div>
+        </>
+        )
+}
 export default VideoItem;
--- myYoutube/src/components/videoList/videoList.jsx
+++ myYoutube/src/components/videoList/videoList.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import VideoItem from '../videoItem/videoItem';
-
+import styles from './videoList.module.css'
 const VideoList = (props) => (
-    <ul>
+    <ul className={styles.videoList}>
+        
         {props.videos.map(video => 
             <VideoItem key={video.id} video={video} />
         )}
