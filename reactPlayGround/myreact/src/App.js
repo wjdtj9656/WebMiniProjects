1f9f40dbcf2482b6b00668b98cf6d75916568814
wjdtj9656 <wjdtj9656@naver.com>
2022-03-30 10:32:24 +0900

    react ex start

---
+++ reactPlayGround/myreact/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+function App() {
+  return (
+    <>
+      <h1>let's get started</h1>
+      <ExpenseItem></ExpenseItem>
+    </>
+  );
+}
+
+export default App;
