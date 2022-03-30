d02a8bd1c88bd787b46621ca2bb9223a009c7702
wjdtj9656 <wjdtj9656@naver.com>
2022-03-30 19:53:09 +0900

    studying props

--- reactPlayGround/myreact/src/App.js
+++ reactPlayGround/myreact/src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import logo from "./logo.svg";
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 function App() {
+  const expenses = [
+    {
+      title: "Car insurance",
+      amount: 294.67,
+      date: new Date(2021, 4, 30).toISOString(),
+    },
+    { title: "Car insurance", amount: 294.67, date: new Date(2022, 4, 30) },
+    { title: "Car insurance", amount: 294.67, date: new Date(2022, 4, 30) },
+  ];
   return (
     <>
       <h1>let's get started</h1>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem val={expenses[0]}></ExpenseItem>
     </>
   );
 }
--- reactPlayGround/myreact/src/components/ExpenseItem.js
+++ reactPlayGround/myreact/src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
-function ExpenseItem() {
-  return <h2>ExpenseItem</h2>;
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <div>{props.val.date}</div>
+      <div className="expense-item__description">
+        <h2>{props.val.title}</h2>
+        <div className="expense-item__price">${props.val.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
