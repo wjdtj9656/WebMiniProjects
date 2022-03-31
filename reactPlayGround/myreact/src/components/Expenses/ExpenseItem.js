73f8ab6261ff0cbb6ea8d5ea254e2bf5a0ef03db
wjdtj9656 <wjdtj9656@naver.com>
2022-04-01 03:55:27 +0900

    use state

--- reactPlayGround/myreact/src/components/Expenses/ExpenseItem.js
+++ reactPlayGround/myreact/src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,23 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
+import { useState } from "react";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.val.title);
+
+  const titleHandler = () => {
+    setTitle("use State");
+    console.log("??");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.val.date} />
       <div className="expense-item__description">
-        <h2>{props.val.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.val.amount}</div>
+        <button onClick={titleHandler}></button>
       </div>
     </Card>
   );
