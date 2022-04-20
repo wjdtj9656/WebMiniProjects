d69ba97a5c4e652b265dd944edf0b5e166b0215c
wjdtj9656 <wjdtj9656@naver.com>
2022-04-20 09:28:57 +0900

    useContext

--- foodex/src/components/Layout/HeaderCartButton.jsx
+++ foodex/src/components/Layout/HeaderCartButton.jsx
@@ -1,14 +1,21 @@
 import styles from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
+import { useContext } from "react";
+import CartContext from "../../store/Cart-context";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfItem = cartCtx.items.reduce((curNum, item) => {
+    return curNum + item.amount;
+  }, 0);
   return (
     <button className={styles.button} onClick={props.onShowChart}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
       <span>Your cart</span>
-      <span className={styles.badge}>3</span>
+      <span className={styles.badge}>{numberOfItem}</span>
     </button>
   );
 };
